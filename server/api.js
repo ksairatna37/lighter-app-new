f9997b94dcfe7642d671cd2fea111bdc9fb9063c
Sairatna Kamble <ksairatna37@gmail.com>
2025-10-07 02:12:36 +0530

    updated version 1 06-10-2025 02:01AM

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+import cors from 'cors';
 
 const app = express();
 
