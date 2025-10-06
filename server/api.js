63dfc9ac13f5564b128af61606fc6b7e5872bca7
Sairatna Kamble <ksairatna37@gmail.com>
2025-10-06 22:43:18 +0530

    updated version 1 06-10-2025 10:43

---
+++ server/api.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+// The "catchall" handler: for any request that doesn't match an API route, send back React's index.html
+app.get('*', (req, res, next) => {
+  if (req.path.includes('/:')) {
+    return res.status(400).send('Malformed URL');
+  }
+
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+});
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
